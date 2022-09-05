02d0e945d43bde3b64ef2e8dcc41a66b3e037289
Moyowaaaa <moyowa7@icloud.com>
2022-09-05 17:14:33 +0100

    [-_-]

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -2,27 +2,12 @@
     import {gsap} from "gsap"
 
 
-    const heroSection = ref<HTMLDivElement>(null)
-    const aboutText = ref<HTMLElement>(null)
+    const aboutSection = ref<HTMLDivElement>()
 
         onMounted(() => {
-            // const tl = gsap.timeline()
-            // tl.to(aboutText.value, { y: 0, opacity: 1, clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)" }, "<0.1");
-
-            gsap.timeline({
-                ScrollTrigger: {
-                    trigger: heroSection,
-                      start: "center center",
-        end: "bottom top",
-        markers: true,
-        scrub: true,
-        pin: true,
-                }
-            })
-             .to(aboutText.value, { y: 0, opacity: 1, clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)" }, "<0.1");
+           console.log(aboutSection.value)
+         
         })
-
-
  
 
 </script>
@@ -30,7 +15,7 @@
 
 
 <template>
-    <div class="h-auto w-full flex flex-col mt-[5rem] "  ref="heroSection">
+    <div class="about h-auto w-full flex flex-col mt-[5rem] "  ref="aboutSection">
 
 
         <div class="h-max lg:h-[600px] lg:px-[5%] ">
@@ -45,7 +30,7 @@
                 <div class=" w-11/12 lg:w-7/12 flex flex-col gap-6 lg:gap-4 ">
 
                  
-                    <h2 class="font-[gotham-bold] font-bold text-3xl" ref="aboutText">Fly It Anywhere</h2>
+                    <h2 class="font-[gotham-bold] font-bold text-3xl">Fly It Anywhere</h2>
                 <p class="lg:w-10/12  text-xl lg:text-2xl font-[gotham-light] font-[400]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et lacus suscipit ex ultrices lobortis. Nullam eget tellus ut massa.</p>
 
                 <div class="bordr-2 border-[teal] w-full flex justify-end font-[gotham-bold]">
--- components/Main.vue
+++ components/Main.vue
@@ -14,13 +14,16 @@
 
         onMounted(() =>{
             const tl = gsap.timeline()
-            tl.fromTo(heroSection.value, { opacity: 0 }, { opacity: 1 });
+            tl.fromTo(heroSection.value, {opacity: 0 }, { opacity: 1 });
+
             tl.to(heroHeader.value, { y: 0, opacity: 1, clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)" }, "<0.1");
             tl.fromTo([heroButtonText.value, heroButtonText2.value], {y: 0, opacity: 0, }, { opacity:1, clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)"}, "<0.1");;
-            // tl.to(heroButtons.value.children, { y: 0, opacity: 1, clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)" }, "<0.1");
+            
             tl.from(heroText.value, {  opacity: 0, y:"-100%",  ease:"power3.easeinOut",}, "<0.1");
-            tl.from(MainDrone.value, {duration:1, opacity:0, ease:"power3.inOut",scale:2.5, }, "<0.1")
-            tl.from(fadedText.value, { opacity:0, y:100, ease:"power2.inOut"}, ".8")
+            tl.from(MainDrone.value, {duration:1, opacity:0, ease:"power3.inOut",scale:2.5,  }, "<0.1")
+   
+
+            console.log(heroText.value)
 
             
         })
@@ -42,7 +45,7 @@
 
             <div class="flex h-full h-full" ref="heroSection">
             <div class="w-full lg:w-8/12 h-full pl-4 lg:pl-20 flex flex-col gap-12 mt-[15vh] lg:mt-0 lg:justify-center">
-              <h1 class="text-4xl lg:text-7xl font-extrabold font-[sans-extra] " ref="heroHeader">Fly With The Birds</h1>  
+              <h1 class="text-4xl lg:text-6xl font-extrabold font-[sans-extra] z-50 " ref="heroHeader">Fly With The Birds</h1>  
               
               
               <p class="font-normal font-[open-sans] text-2xl" ref="heroText">Go beyond the mountains and into the valleys</p>
@@ -71,8 +74,8 @@
         </div>
 
 
-        <div class="absolute fade pb-0 lg:pb-3" ref="fadedText">
-            <h2 class="text-6xl font-black text-black">Droves</h2>
+        <div class="absolute fade pb-0 lg:pb-3" >
+            <h2 class="text-6xl font-black text-black" ref="fadedText">Droves</h2>
         </div>
         </div>
 
