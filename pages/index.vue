d815ae83d4f82b03b85a6bdb3b6825eac4b2ea19
Moyowaaaa <moyowa7@icloud.com>
2023-01-19 23:26:58 +0100

    test flight

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<div class="">
+	<div class="" v-if="assetsLoaded">
 		<Main />
 		 <AboutSection/>
 		<StoreSection />
@@ -11,6 +11,31 @@
 </template>
 
 <script setup lang="ts">
+import mainBackground from '../assets/images/mainbackground.png'
+import mainDrone from '../assets/images/main.svg'
+import tooltip from '~/assets/images/tooltip.svg'
+
+
+const assetsLoaded = ref<boolean>(false)
+
+const assets = ref([mainDrone,tooltip, mainBackground])
+
+onMounted(() => {
+	const promises:any[] = []
+    assets.value.forEach(asset => {
+        const img = new Image()
+        img.src = asset
+        promises.push(new Promise((resolve, reject) => {
+          img.onload = resolve
+          img.onerror = reject
+        }))        
+    })
+        
+    Promise.all(promises).then(() => {
+        assetsLoaded.value = true
+      })
+
+})
 
 </script>
 
